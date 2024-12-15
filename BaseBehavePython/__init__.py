fb800ad00e8bd5e92381d7134f50eef707570995
Sathya <sathyas0811@gmail.com>
2024-12-15 10:50:24 +0800

    updated base

---
+++ BaseBehavePython/__init__.py
@@ -0,0 +1,2 @@
+from BaseBehavePython.pages.BasePage import *
+from BaseBehavePython.features.environment import driver_initialization, launchApplication, driver_close, logger_Predefined
--- build/lib/BaseBehavePython/utilities/configReader.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-
-def read_configuration(category, key):
-    config = ConfigParser()
-    config.read("BaseBehavePython/configurations/config.ini")
-    return config.get(category, key)
