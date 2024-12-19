35fe4c3afc91e679f15d3bc34bc622a116dd9080
Sathya <sathyas0811@gmail.com>
2024-12-19 08:12:00 +0800

    License Update 5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
